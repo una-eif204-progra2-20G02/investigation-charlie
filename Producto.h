fbc170d4b0dd8de12c8831de9e9337c9f03cccb5
Diego Sanchez <diegopasj@gmail.com>
2020-08-24 01:52:25 +0000

    Añadidas clase Producto y ProductoDigital

---
+++ Producto.h
@@ -0,0 +1,13 @@
+#ifndef INVES_LISKOV
+#define INVES_LISKOV
+#include <sstream>
+
+class Producto{
+private:
+std::string name;
+public:
+virtual std:string toString();
+
+
+};
+#endif //INVES_LISKOV
