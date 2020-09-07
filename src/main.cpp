150eeca8c227c8bb98fa4f1eaf4d6ae1ffba6271
Diegex30 <diegopasj@gmail.com>
2020-09-07 15:31:28 -0600

    Main update

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,6 @@
 #include "MagicCard.h"
 #include "Deck.h"
 
-   
-    //Mal ejemplo de Lizcov
-struct Animal{
-    Animal(){}
-
-    virtual std::string PerroCome()=0;
-
-    std::string hablar(Animal* ani){ // expandir el codigo con mas animales requiere expandir este metodo
-    if (Perro* per = dynamic_cast<Perro*>(ani)) {
-        return per->ladrar();
-    }
-    if (Gato* per = dynamic_cast<Gato*>(ani)) {
-        return per->maullar();
-    }
-}
-};
-struct Perro: Animal{
-    Perro(){}
-    std::string ladrar(){ // solo los perros ladran
-        return "woof";
-    }
-    virtual std::string PerroCome(){
-    return "El perro come";
-    }
-};
-struct Gato: Animal{
-    Gato(){}
-    std::string maullar(){ // solo los gatos maullan
-        return "miau";
-    }
-    virtual std::string PerroCome(){}
-};
-
-
-
-
-
 int main(){
 
     MonsterCard card;// = new Card();
