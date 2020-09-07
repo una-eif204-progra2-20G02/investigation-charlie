2c3663fd722edb0bb81b9c845f218af2eec5d5b4
Diegex30 <diegopasj@gmail.com>
2020-09-07 15:30:55 -0600

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,14 @@
 #include "MonsterCard.h"
 #include "MagicCard.h"
 #include "Deck.h"
-/*
-    // buen ejemplo de Liskov
-struct Animal{
-    Animal(){}
-    virtual std::string hablar(){   //cualquier animal que se implemente debe ser capaz de hablar
-        return " hola ";
-    }
-};
-struct Perro: Animal{
-    Perro(){}
-    std::string hablar() override{ // el perro sobreescribe el metodo para poder ladrar
-    return "woof";
-}
-};
 
-struct Gato: Animal{
-    Gato(){}
-    std::string hablar() override{ // el gato sobreescribe el metodo para poder maullar
-    return "miau";
-}
-};
-*/
-
-/*
+   
     //Mal ejemplo de Lizcov
 struct Animal{
     Animal(){}
-    virtual std::string PerroCome(){
-    return "El perro come";
-    }
+
+    virtual std::string PerroCome()=0;
+
     std::string hablar(Animal* ani){ // expandir el codigo con mas animales requiere expandir este metodo
     if (Perro* per = dynamic_cast<Perro*>(ani)) {
         return per->ladrar();
@@ -39,7 +17,7 @@ struct Animal{
     if (Gato* per = dynamic_cast<Gato*>(ani)) {
         return per->maullar();
     }
-}// (sin polimorfismo no puedo usar 'dynamic_cast' en el metodo hablar)
+}
 };
 struct Perro: Animal{
     Perro(){}
@@ -61,7 +39,7 @@ struct Gato: Animal{
 
 
 
-*/
+
 int main(){
 
     MonsterCard card;// = new Card();
